61fdd2347c3241d0a91b21ebebd8fc7c398d7360
Tejas Singh <95495584+ec-026@users.noreply.github.com>
2023-09-23 19:24:51 +0530

    Update yt.sh

--- yt.sh
+++ yt.sh
@@ -1,20 +1,30 @@
-#! /bin/bash
+#!/bin/bash
 
-VBR="400k"
-FPS="20"
-QUAL="medium"
+VBR="1500k"
+FPS="24"
+QUAL="superfast"
 
 YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
-KEY="vzja-257b-z45a-9ua3-2f19"
+KEY="YOUR API KEY HERE"
 
-VIDEO_SOURCE="/home/runner/ytstream/pixel-jeff-mario.gif"
-TEXT="/home/runner/ytstream/input.txt"
+VIDEO_SOURCE="/home/ubuntu/pixel-jeff-mario.gif"
+AUDIO_SOURCE="/home/ubuntu/input.txt"
 
-ffmpeg \
-    -re -f lavfi -i "movie=filename=$VIDEO_SOURCE:loop=0, setpts=N/(FRAME_RATE*TB), pad=ceil(iw/2)*2:ceil(ih/2)*2" \
-    -re -f concat -i $TEXT \
-    -map 0:v:0 -map 1:a:0 \
-    -map_metadata:g 1:g \
-    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
-    -acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
-    -f flv "$YOUTUBE_URL/$KEY"
+# Initialize the playlist position to 1
+playlist_position=1
+
+while true; do
+    echo "Stream ended. Restarting...."
+    pkill -f "ffmpeg"
+    sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+    # Start ffmpeg streaming with the current playlist position
+    ffmpeg \
+        -re -f lavfi -i "movie=filename=$VIDEO_SOURCE:loop=0, setpts=N/(FRAME_RATE*TB)" \
+        -thread_queue_size 512 \
+        -f concat -safe 0 -stream_loop -1 -i "$AUDIO_SOURCE" -ss 0 -t 9999999 \
+        -map 0:v:0 -map 1:a:0 \
+        -map_metadata:g 1:g \
+        -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+        -acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
+        -f flv "$YOUTUBE_URL/$KEY"
+done
